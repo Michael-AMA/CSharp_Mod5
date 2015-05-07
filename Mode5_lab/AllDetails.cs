66cf7bdf116b65a5da26c403be93fe4db5823613
michaelAMA <joebob60@hotmail.com>
2015-05-06 18:38:08 -0700

    Uploaded Module 5 assignment

---
+++ Mode5_lab/AllDetails.cs
@@ -0,0 +1,49 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+
+//namespace Mode5_lab
+//{
+
+//    class AllDetails
+//    {
+        
+//        struct StudentInfo
+//        {
+//            private string _studentName;
+
+//            public string StudentName
+//            {
+//                get { return _studentName; }
+//                set { _studentName = value; }
+//            }
+
+//            private string _birthday;
+
+//            public string Birthday
+//            {
+//                get { return _birthday; }
+//                set { _birthday = value; }
+//            }
+//        }
+        
+//        private string _teacherInfo;
+
+//        public string TeacherInfo
+//        {
+//            get { return _teacherInfo; }
+//            set { _teacherInfo = value; }
+//        }
+//        private string _uProgram;
+
+//        public string UProgram
+//        {
+//            get { return _uProgram; }
+//            set { _uProgram = value; }
+//        }
+
+
+//    }
+//}
